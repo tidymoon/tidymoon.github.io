eeb813f9d0adcfd6107cc42da42387242cc034da
小魏 <wmyqqyx@qq.com>
2020-09-17 20:16:46 +0800

    提交项目

---
+++ tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64d632efaee45188028b04a2add7e531";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
