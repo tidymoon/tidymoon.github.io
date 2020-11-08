0418740ae26489ef2456a89df49eace9019161ee
tidymoon <73995927+tidymoon@users.noreply.github.com>
2020-11-08 16:28:08 +0800

    add newpage

--- js/tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?0d0529dbaef7441219b56c8eedec4eab";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
