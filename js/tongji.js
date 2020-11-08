177c0e40836cce16fefd8463590ffaeabacf4477
小魏 <wmyqqyx@qq.com>
2020-11-08 10:15:58 +0800

    add newpage

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0d0529dbaef7441219b56c8eedec4eab";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
